d404beaacfd7164fa07375e2ccef417f21ebda0a
peace1nmind <aont95@naver.com>
2024-07-12 11:33:17 +0900

    댓글 Dto 제작

--- src/main/java/com/peace1nmind/d0708/dto/CommentDto.java
+++ src/main/java/com/peace1nmind/d0708/dto/CommentDto.java
@@ -11,5 +11,68 @@ public class CommentDto {
 	private int good;
 	private int bad;
 	
+	
+	public CommentDto(){
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public CommentDto(String boardnum, String nickname, String comments, String commentdate, int good, int bad) {
+		super();
+		this.boardnum = boardnum;
+		this.nickname = nickname;
+		this.comments = comments;
+		this.commentdate = commentdate;
+		this.good = good;
+		this.bad = bad;
+	}
+
+	public String getBoardnum() {
+		return boardnum;
+	}
+
+	public void setBoardnum(String boardnum) {
+		this.boardnum = boardnum;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getComments() {
+		return comments;
+	}
+
+	public void setComments(String comments) {
+		this.comments = comments;
+	}
+
+	public String getCommentdate() {
+		return commentdate;
+	}
+
+	public void setCommentdate(String commentdate) {
+		this.commentdate = commentdate;
+	}
+
+	public int getGood() {
+		return good;
+	}
+
+	public void setGood(int good) {
+		this.good = good;
+	}
+
+	public int getBad() {
+		return bad;
+	}
+
+	public void setBad(int bad) {
+		this.bad = bad;
+	}
 
 }
